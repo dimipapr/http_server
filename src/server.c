359c714abe02d9b4519e9a185d99e0bad271f7d6
Dimitris Papadimitriou <dimipapr@gmail.com>
2024-12-18 19:05:17 +0200

    rellocation

--- src/server.c
+++ src/server.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 #include <signal.h>
 
-void sigintHandler(int signo){
-    if( signo == SIGINT ){
-        printf("Shutting down...\n");
-        exit(EXIT_FAILURE);
-    }
-}
+void sigintHandler(int signo);
 
 int main(){
     signal(SIGINT, sigintHandler);
     for(;;);
 
     return 0;
+}
+
+void sigintHandler(int signo){
+    if( signo == SIGINT ){
+        printf("Shutting down...\n");
+        exit(EXIT_FAILURE);
+    }
 }
