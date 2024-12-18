40aa02d3596daae48ef9a4b1e5aa14852ef49252
Dimitris Papadimitriou <dimipapr@gmail.com>
2024-12-18 19:04:10 +0200

    added SIGINT handler

--- src/server.c
+++ src/server.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+
+void sigintHandler(int signo){
+    if( signo == SIGINT ){
+        printf("Shutting down...\n");
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main(){
-    printf("Hi\n");
+    signal(SIGINT, sigintHandler);
+    for(;;);
+
     return 0;
 }
