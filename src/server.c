97993b16d6edf53b73836e91394984767925f92e
Dimitris Papadimitriou <dimipapr@gmail.com>
2024-12-30 16:07:46 +0200

    created initServer() function

--- src/server.c
+++ src/server.c
@@ -10,39 +10,22 @@
 
 #define RECEIVE_BUFFER_SIZE 1024
 
-uint16_t parse_input(int argc, char *argv[]);
+uint16_t parseInput(int argc, char *argv[]);
+int initServer(uint16_t port);
+
+const uint16_t INCOMMING_CONNECTION_QUEUE_MAX = 10;
 
 int main(int argc, char *argv[]){
-    const uint16_t INCOMMING_CONNECTION_QUEUE_MAX = 10;
     uint16_t port;
     //parse input
-    port = parse_input(argc, argv);
+    port = parseInput(argc, argv);
     
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int server_fd = initServer(port);
     if (server_fd == -1){
-        printf("Error creating socket : %s\n",strerror(errno));
+        printf("Error initializing server: %s\n",strerror(errno));
         exit(EXIT_FAILURE);
     }
-    //bind
-    struct sockaddr_in sockaddr_in_server;
-    sockaddr_in_server.sin_family = AF_INET;
-    sockaddr_in_server.sin_addr.s_addr = htonl(INADDR_ANY);
-    sockaddr_in_server.sin_port = htons(port);
-    socklen_t sockaddr_in_server_length = sizeof(sockaddr_in_server);
     
-    int result = bind(server_fd, (struct sockaddr *)&sockaddr_in_server, sockaddr_in_server_length);
-    if (result == -1){
-        printf("Error binding to address:%s\n",strerror(errno));
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
-
-    result = listen(server_fd, INCOMMING_CONNECTION_QUEUE_MAX);
-    if ( result == -1 ){
-        printf("Error listen():%s\n",strerror(errno));
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
 
     printf("Listening on port %u\n",port);
 
@@ -84,7 +67,7 @@ int main(int argc, char *argv[]){
 
 }
 
-uint16_t parse_input(int argc, char *argv[]){
+uint16_t parseInput(int argc, char *argv[]){
     if (argc > 2){
         printf("USAGE: ./server [port(default=8000)]");
         exit(EXIT_FAILURE);
@@ -93,4 +76,28 @@ uint16_t parse_input(int argc, char *argv[]){
     }else{
         return 8000;
     }
+}
+
+int initServer(uint16_t port){
+    //create socket
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1)return -1;    //bind
+    struct sockaddr_in sockaddr_in_server;
+    sockaddr_in_server.sin_family = AF_INET;
+    sockaddr_in_server.sin_addr.s_addr = htonl(INADDR_ANY);
+    sockaddr_in_server.sin_port = htons(port);
+    socklen_t sockaddr_in_server_length = sizeof(sockaddr_in_server);
+    
+    int result = bind(server_fd, (struct sockaddr *)&sockaddr_in_server, sockaddr_in_server_length);
+    if (result == -1){
+        close(server_fd);
+        return -1;
+    }
+
+    result = listen(server_fd, INCOMMING_CONNECTION_QUEUE_MAX);
+    if (result == -1){
+        close(server_fd);
+        return -1;
+    }
+    return server_fd;
 }
